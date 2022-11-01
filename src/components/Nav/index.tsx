41107c82f895153db6f5df5b8acaad33959d9faf
Nghi Nguyen <nghi.nguyen@evehr.com>
2022-11-01 23:34:39 +0700

    Feat: layout - create nav with grid + flex

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { BiHomeAlt, BiMoviePlay, BiInfoCircle } from 'react-icons/bi'
+import NavItem from './NavItem';
+
+const defaultIconSize = '1.875rem';
+
+const items = [
+  {label: 'Home', icon: <BiHomeAlt size={defaultIconSize} />, active: true},
+  {label: 'Movies', icon: <BiMoviePlay size={defaultIconSize} />},
+  {label: 'About', icon: <BiInfoCircle size={defaultIconSize} />}
+]
+
+const NavItemContainer = () => (
+  <>
+    {items.map((item, index) => <NavItem item={item} key={index} />)}
+  </>
+)
 
 const Nav = () => {
   return (
     <nav className='col-span-1 bg-cyan-200'>
-      Nav
+      <div className='mx-4'>
+        <h4 className='uppercase font-bold text-primary py-4 border-primary border-b text-right'>
+          Phimmoi.net
+        </h4>
+      </div>
+
+      <ul className='mx-4 my-2'>
+        <NavItemContainer />
+      </ul>
     </nav>
   )
 }

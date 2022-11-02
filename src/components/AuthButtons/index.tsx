29706acfa6cb5830dab260fc930f88e8f6433e66
Nghi Nguyen <nghi.nguyen@evehr.com>
2022-11-02 07:32:49 +0700

    Feat: component - create Header & AuthButton component

---
+++ src/components/AuthButtons/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div className='flex justify-end'>
+      <button className='btn mr-2'>
+        Login
+      </button>
+      <button className='btn'>
+        Register
+      </button>
+    </div>
+  )
+}
+
+export default Index
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <h1 className="mt-6 text-cyan-900">
+      #1 Free Online Movie Streaming Site In Vietnam
+    </h1>
+  )
+}
+
+export default Index
